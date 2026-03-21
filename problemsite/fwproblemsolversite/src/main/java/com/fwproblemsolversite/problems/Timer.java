d659fe1c84b124aa07935de50ccacf0a52c37b3b
SHearrell <hearrell@email.sc.edu>
2026-03-20 22:37:58 -0400

    Updated Progress class to track user progress and added the daily streak feature also javadoc'd

--- problemsite/fwproblemsolversite/src/main/java/com/fwproblemsolversite/problems/Timer.java
+++ problemsite/fwproblemsolversite/src/main/java/com/fwproblemsolversite/problems/Timer.java
@@ -9,18 +9,24 @@ public class Timer {
     private LocalTime stopTime;
     private double elapsedTime;
     private double timeLimit;
-
+    
+    /**
+     * Constructor for the Timer class
+     * @param time The time limit for the timer
+     */
     public Timer(Double time) {
         this.timeLimit = (time == null) ? 0.0 : time;
         this.elapsedTime = 0.0;
     }
 
+    // Method to start the timer
     public void start() {
         startTime = LocalTime.now();
         stopTime = null;
         elapsedTime = 0.0;
     }
 
+    // Method to stop the timer and calculate elapsed time
     public void stop() {
         if (startTime == null) {
             elapsedTime = 0.0;
@@ -34,10 +40,12 @@ public class Timer {
         return elapsedTime;
     }
 
+    // Method to check if the elapsed time has exceeded the time limit
     public boolean isOverLimit() {
         return timeLimit > 0 && elapsedTime > timeLimit;
     }
 
+    // Method to convert the time limit to a double value
     public double toDouble() {
         return timeLimit;
     }
