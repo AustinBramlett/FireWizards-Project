9c51cae44f8fe565b117c5a78fe612ffcc7e4005
AustinBramlett <austingoldenboy30@gmail.com>
2026-03-23 19:11:04 -0400

    Fixed problems.json not having ids as well as comments

--- problemsite/fwproblemsolversite/src/main/java/com/fwproblemsolversite/drivers/dataLoader.java
+++ problemsite/fwproblemsolversite/src/main/java/com/fwproblemsolversite/drivers/dataLoader.java
@@ -66,7 +66,7 @@ public class dataLoader {
             for(Object obj : jsonObject) {
                 JSONObject problemObj = (JSONObject) obj;
                 // Parses the JSON and create Problem instances.
-                UUID id = (String) problemObj.get("problemID") != null ? UUID.fromString((String) problemObj.get("problemID")) : null;
+                UUID id = (String) problemObj.get("problemID") != null ? UUID.fromString((String) problemObj.get("problemID")) : UUID.randomUUID();
                 String title = (String) problemObj.get("title");
                 String description = (String) problemObj.get("description");
                 String difficultyString = (String) problemObj.get("difficulty");
