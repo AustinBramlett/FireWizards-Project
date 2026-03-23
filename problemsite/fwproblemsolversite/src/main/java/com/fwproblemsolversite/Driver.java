05418d1c006574bf88ac039fe0d7dfbfbd1bb362
AustinBramlett <austingoldenboy30@gmail.com>
2026-03-22 23:32:48 -0400

    added sally scenario test in driver

--- problemsite/fwproblemsolversite/src/main/java/com/fwproblemsolversite/Driver.java
+++ problemsite/fwproblemsolversite/src/main/java/com/fwproblemsolversite/Driver.java
@@ -24,8 +24,82 @@ public class Driver {
         } else {
             System.out.println("Login failed.");
         }
-        //ADD PROBLEM TESTS HERE
+        //SALLY SCENARIO
+        System.out.println("\n--- Sally's Scenario ---");
+
+        boolean duplicate = app.createAccount(
+            "Sally", "Sparrow","ssparrow","sally@email.com", "pass123"
+        );
+
+        if(!duplicate){
+            System.out.println("Duplicate account rejected");
+        }
+
+        //Create new account (different username)
+        boolean created = app.createAccount(
+            "Sally", "Sparrow", "ssparrow2", "sally@email.com", "pass123"
+        );
+
+        if (created){
+            System.out.println("New account created successfully.");
+        } else {
+            System.out.println("Account creation failed.");
+        }
+        // Login 
+        Account sally = app.login("ssparrow2", "pass123");
+        System.out.println("Welcome " + sally.getUsername() + " to the system!");
+
+        //constraints
+        ArrayList<String> constraints = new ArrayList<>();
+        constraints.add("Array can contain negative numbers");
+
+        //examples
+        ArrayList<ArrayList<String>> examples = new ArrayList<>();
+
+        ArrayList<String> example1 = new ArrayList<>();
+        example1.add("[1, -1, 5, -2, 3], k = 3");
+        example1.add("4");
+
+        ArrayList<String> example2 = new ArrayList<>();
+        example2.add("[-2, -1, 2, 1], k = 3");
+        example2.add("2");
+
+        examples.add(example1);
+        examples.add(example2);
+
+        //notes 
+        ArrayList<String> notes = new ArrayList<>();
+        notes.add("Brute Force )(n^2)");
+        notes.add("HAshMap 0(n)");
+
+        //tags
+        ArrayList<String> tags = new ArrayList<>();
+        tags.add("Array");
+        tags.add("HashMap");
         
+        //add problem
+        app.addProblem(
+            "Longest Subarray with Sum K",
+            "Find the length of the longest contiguous subarray whose sum equals k.",
+            constraints,
+            com.fwproblemsolversite.enums.Language.JAVA,
+            examples,
+            notes,
+            com.fwproblemsolversite.enums.ProblemType.ARRAY,
+            tags,
+            30.0,
+            "bruteforce.java, hashmap.java",
+            com.fwproblemsolversite.enums.Difficulty.MEDIUM
+        );
+
+        //show the problems 
+        for (Problem p : app.getAllQuestions()){
+            System.out.println(p.getTitle());
+        }
+        //logout 
+        app.logout();
+        System.out.println("User logged out successfully.");
+
         //Test the getAllQuestions function to ensure that the questions are being loaded correctly.
         ArrayList<Problem> questions = app.getAllQuestions();
 
@@ -54,5 +128,13 @@ public class Driver {
 
         //Logout the user to ensure that the logout function works correctly.
         app.logout();
+
+        com.fwproblemsolversite.drivers.dataWriter writer =
+        new com.fwproblemsolversite.drivers.dataWriter();
+
+        writer.saveAccounts(app.getAccountData().getAccounts());
+        writer.saveProblems(app.getAllQuestions());
+        
     }
+    
 }
