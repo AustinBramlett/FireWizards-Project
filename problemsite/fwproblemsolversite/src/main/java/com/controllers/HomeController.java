a8b22603378f633fa8d8679d9ce6f4fa5b4d0fb1
SHearrell <hearrell@email.sc.edu>
2026-04-25 20:51:18 -0400

    Updated Home UI to look like figma design, when clicking learn more it show the README info, and increase login card size.

--- problemsite/fwproblemsolversite/src/main/java/com/controllers/HomeController.java
+++ problemsite/fwproblemsolversite/src/main/java/com/controllers/HomeController.java
@@ -2,10 +2,32 @@ package com.controllers;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 import com.fwproblemsolversite.App;
 
 public class HomeController {
+
+    @FXML private VBox welcomeBox;
+    @FXML private VBox readmeBox;
+
+    @FXML
+    private void handleLearnMore() {
+        welcomeBox.setVisible(false);
+        welcomeBox.setManaged(false);
+
+        readmeBox.setVisible(true);
+        readmeBox.setManaged(true);
+    }
+
+    @FXML
+    private void handleBackToWelcome() {
+        readmeBox.setVisible(false);
+        readmeBox.setManaged(false);
+
+        welcomeBox.setVisible(true);
+        welcomeBox.setManaged(true);
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
